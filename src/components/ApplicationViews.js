5a4f0e5ddeab8d218f9855a2c6feae60f2f1bc91
Noah Taggart <noahtag@gmail.com>
2022-04-29 13:32:26 -0500

    init commit

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Route } from "react-router-dom"
+
+
+export const ApplicationViews = () => {
+    return <>
+        <main style={{
+            margin: "5rem 2rem",
+            backgroundColor: "lightgoldenrodyellow",
+            lineHeight: "1.75rem"
+        }}>
+
+
+        </main>
+    </>
+}
